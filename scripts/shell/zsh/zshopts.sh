eaf2ddfb360dea80a2442c8043c0170662ef22ec
arilebedey <34816154+arilebedey@users.noreply.github.com>
2024-11-21 00:14:35 +0100

    tmux config, java 4 android, bt setting

--- scripts/shell/zsh/zshopts.sh
+++ scripts/shell/zsh/zshopts.sh
@@ -8,3 +8,9 @@ SAVEHIST=30000
 XDG_CONFIG_HOME=/home/ari/.config
 # writes to history from all terminals as above, but that history "will not be available immediately from other instances of the shell that are using the same history file"
 setopt INC_APPEND_HISTORY_TIME
+# Java for RN
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk/
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/tools
--- scripts/ui/launch_tmux.sh
+++ scripts/ui/launch_tmux.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-tmux new-session -d \; source-file /home/ari/System/dotfiles/.config/tmux/startup-appendix.conf \; attach
+SESSION_NAME="home"
+
+# Check if the session exists
+if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+  # Attach to the existing session
+  tmux attach-session -t "$SESSION_NAME"
+else
+  # Create a new session with the specified name and run the configuration file
+  tmux new-session -d -s "$SESSION_NAME" \; source-file /home/ari/System/dotfiles/.config/tmux/startup-appendix.conf \; attach-session -t "$SESSION_NAME"
+fi
+
