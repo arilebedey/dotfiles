d55c0c3d01d1c0964336cdcf671d80ab60fa2367
arilebedey <34816154+arilebedey@users.noreply.github.com>
2025-07-12 23:27:38 +0200

    M1: tmux + 42formatter nvim

--- dotfiles/.config/nvim/lua/ari/plugins/42formatting.lua
+++ dotfiles/.config/nvim/lua/ari/plugins/42formatting.lua
@@ -1,43 +1,19 @@
 return {
   "MoulatiMehdi/42norm.nvim",
-  ft = { "c", "cpp" }, -- Only load for C and C++ files
+  ft = { "c", "cpp" },
   config = function()
+    if vim.loop.os_uname().sysname == "Darwin" then
+      local venv_path = "/Users/ari/.venv/norminette-venv/bin"
+      vim.env.PATH = venv_path .. ":" .. vim.env.PATH
+    end
+
     local norm = require("42norm")
 
     norm.setup({
-      header_on_save = true,     -- Insert/update 42 header on save
-      format_on_save = true,     -- Format code on save
-      linter_on_change = true,   -- Update diagnostics when buffer changes
-      timeout = 3000,            -- Timeout for norminette (in milliseconds)
+      header_on_save = true,   -- Insert/update 42 header on save
+      format_on_save = true,   -- Format code on save
+      linter_on_change = true, -- Update diagnostics when buffer changes
+      timeout = 4000,          -- Timeout for norminette (in milliseconds)
     })
-
-    -- Key mappings
-    -- F5 to run norminette and check norms
-    vim.keymap.set("n", "<F5>", function()
-      norm.check_norms()
-    end, { desc = "Update 42norms diagnostics", noremap = true, silent = true })
-
-    -- Ctrl+F to format the buffer (or choose another key if this conflicts)
-    vim.keymap.set("n", "<C-f>", function()
-      norm.format()
-    end, { desc = "Format buffer on 42norms", noremap = true, silent = true })
-
-    -- F1 to add/update the 42 header (or choose another key if this conflicts)
-    vim.keymap.set("n", "<F1>", function()
-      norm.stdheader()
-    end, { desc = "Insert 42header", noremap = true, silent = true })
-
-    -- Create convenient commands
-    vim.api.nvim_create_user_command("Norminette", function()
-      norm.check_norms()
-    end, {})
-    
-    vim.api.nvim_create_user_command("Format42", function()
-      norm.format()
-    end, {})
-    
-    vim.api.nvim_create_user_command("Stdheader", function()
-      norm.stdheader()
-    end, {})
   end,
 }
