3baf7480355dc2371a8ce618de7d375c58c1b4ee
arilebedey <34816154+arilebedey@users.noreply.github.com>
2025-10-29 20:02:14 +0100

    new formatting 42 rules for zmk configs

--- dotfiles/.config/nvim/lua/ari/plugins/42formatting.lua
+++ dotfiles/.config/nvim/lua/ari/plugins/42formatting.lua
@@ -10,8 +10,8 @@ return {
     local norm = require("42norm")
 
     norm.setup({
-      header_on_save = true,   -- Insert/update 42 header on save
-      format_on_save = true,   -- Format code on save
+      -- header_on_save = true,   -- Insert/update 42 header on save
+      -- format_on_save = true,   -- Format code on save
       linter_on_change = true, -- Update diagnostics when buffer changes
       timeout = 4000,          -- Timeout for norminette (in milliseconds)
     })
