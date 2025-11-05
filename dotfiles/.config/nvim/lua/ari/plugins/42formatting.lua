c36405c45363b219050cd5142ffcbf3b8b3844b4
arilebedey <34816154+arilebedey@users.noreply.github.com>
2025-11-05 08:35:14 +0100

    gitconfig: cleanup

--- dotfiles/.config/nvim/lua/ari/plugins/42formatting.lua
+++ dotfiles/.config/nvim/lua/ari/plugins/42formatting.lua
@@ -11,9 +11,9 @@ return {
 
     norm.setup({
       -- header_on_save = true,   -- Insert/update 42 header on save
-      -- format_on_save = true,   -- Format code on save
+      format_on_save = true,   -- Format code on save
       linter_on_change = true, -- Update diagnostics when buffer changes
-      timeout = 4000,          -- Timeout for norminette (in milliseconds)
+      timeout = 500,           -- Timeout for norminette (in milliseconds)
     })
   end,
 }
