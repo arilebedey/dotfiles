86f61fdd9e32f880db727292065c8d49897e2838
arilebedey <34816154+arilebedey@users.noreply.github.com>
2026-01-18 09:12:33 +0100

    dark bg edge

--- dotfiles/.config/nvim/lua/ari/core/themes/edge.lua
+++ dotfiles/.config/nvim/lua/ari/core/themes/edge.lua
@@ -1,9 +1,14 @@
 local M = {}
 M.config = function()
-  vim.g.edge_style = "neon"
-  vim.cmd("colorscheme edge")
-  vim.cmd('set laststatus=0')
+	vim.g.edge_style = "neon"
+	vim.g.edge_colors_override = {
+		bg0 = { "#22202b", "234" },
+		bg1 = { "#292530", "235" },
+		bg2 = { "#302c38", "236" },
+	}
+	vim.cmd("colorscheme edge")
+	vim.cmd("set laststatus=0")
 end
 return {
-  { "sainnhe/edge", priority = 1000, config = M.config }
+	{ "sainnhe/edge", priority = 1000, config = M.config },
 }
